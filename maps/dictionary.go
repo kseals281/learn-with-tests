46a63f26fbd69d44b1257e07f87babcc27f97b10
Khari Seals <kseals281@gmail.com>
2020-03-30 19:15:20 -0400

    Added full CRUD operations for the dictionary. Changed errors from constant strings to error wrappers.

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,26 +1,49 @@
 package maps
 
 const (
-	ErrWordNotFound = DictionaryErr("unable to find word in dictionary")
-	ErrWordExists   = DictionaryErr("word already exists in dictionary")
+	ErrWordNotFound     = DictionaryErr("unable to find word in dictionary")
+	ErrWordExists       = DictionaryErr("word already exists in dictionary")
+	ErrWordDoesNotExist = DictionaryErr("word does not exist within the dictionary")
 )
 
 type DictionaryErr string
 
 type Dictionary map[string]string
 
-func (d Dictionary) Search(term string) (string, DictionaryErr) {
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+func (d Dictionary) Search(term string) (string, error) {
 	if m, ok := d[term]; !ok {
 		return "", ErrWordNotFound
 	} else {
-		return m, ""
+		return m, nil
 	}
 }
 
-func (d Dictionary) add(term, definition string) DictionaryErr {
+func (d Dictionary) Add(term, definition string) error {
 	if _, ok := d[term]; ok {
 		return ErrWordExists
 	}
 	d[term] = definition
-	return ""
+	return nil
+}
+
+func (d Dictionary) Update(term, definition string) error {
+	_, err := d.Search(term)
+	if err != nil {
+		return ErrWordDoesNotExist
+	}
+	d[term] = definition
+	return nil
+}
+
+func (d Dictionary) Delete(term string) error {
+	_, err := d.Search(term)
+	if err != nil {
+		return ErrWordDoesNotExist
+	}
+	delete(d, term)
+	return nil
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func ErrCheck(t *testing.T, err DictionaryErr, want DictionaryErr) {
+func ErrCheck(t *testing.T, err error, want error) {
 	t.Helper()
 	if err != want {
 		t.Errorf("Got unexpected error %v, want %v", err, want)
@@ -18,14 +18,14 @@ func TestSearch(t *testing.T) {
 		dictionary Dictionary
 		term       string
 		wantString string
-		wantErr    DictionaryErr
+		wantErr    error
 	}{
 		{
 			"Single known word",
 			Dictionary{"test": "this is just a test"},
 			"test",
 			"this is just a test",
-			"",
+			nil,
 		}, {
 			"Unknown word",
 			Dictionary{},
@@ -36,7 +36,7 @@ func TestSearch(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got, err := tt.dictionary.Search(tt.term); err != "" {
+			if got, err := tt.dictionary.Search(tt.term); err != nil {
 				ErrCheck(t, err, tt.wantErr)
 			} else if !reflect.DeepEqual(got, tt.wantString) {
 				t.Errorf("got %s, want %s", got, tt.wantString)
@@ -54,16 +54,16 @@ func TestAdd(t *testing.T) {
 		name    string
 		d       Dictionary
 		args    args
-		wantErr DictionaryErr
+		wantErr error
 	}{
 		{
-			"Add successful",
+			"New word",
 			Dictionary{},
 			args{
 				term:       "foo",
 				definition: "foo bar baz",
 			},
-			"",
+			nil,
 		}, {
 			"Word exists",
 			Dictionary{"foo": ""},
@@ -76,13 +76,89 @@ func TestAdd(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := tt.d.add(tt.args.term, tt.args.definition); err != tt.wantErr {
+			if err := tt.d.Add(tt.args.term, tt.args.definition); err != tt.wantErr {
 				ErrCheck(t, err, tt.wantErr)
 			}
 			if def, ok := tt.d[tt.args.term]; !ok {
 				if !reflect.DeepEqual(def, tt.args.definition) {
 					t.Errorf("Wanted def: %s\nGot def: %s", tt.args.definition, def)
 				}
+			}
+		})
+	}
+}
+
+func TestUpdate(t *testing.T) {
+	type args struct {
+		term       string
+		definition string
+	}
+	tests := []struct {
+		name    string
+		d       Dictionary
+		args    args
+		want    error
+		wantDef string
+	}{
+		{
+			"word exists",
+			Dictionary{"foo": ""},
+			args{
+				term:       "foo",
+				definition: "bar",
+			},
+			nil,
+			"bar",
+		}, {
+			"word doesn't exist",
+			Dictionary{},
+			args{
+				term:       "foo",
+				definition: "bar",
+			},
+			ErrWordDoesNotExist,
+			"",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			term := tt.args.term
+			if got := tt.d.Update(term, tt.args.definition); got != tt.want {
+				t.Errorf("update() = %v, want %v", got, tt.want)
+			}
+			if definition := tt.d[term]; !reflect.DeepEqual(tt.wantDef, definition) {
+				t.Errorf("expected definition %s, got %s", tt.wantDef, definition)
+			}
+		})
+	}
+}
+
+func TestDelete(t *testing.T) {
+	tests := []struct {
+		name    string
+		d       Dictionary
+		term    string
+		wantErr error
+	}{
+		{
+			"Word exists",
+			Dictionary{"foo": ""},
+			"foo",
+			nil,
+		}, {
+			"Word does not exist",
+			Dictionary{},
+			"foo",
+			ErrWordDoesNotExist,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := tt.d.Delete(tt.term); err != tt.wantErr {
+				t.Errorf("Delete() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if _, ok := tt.d[tt.term]; ok {
+				t.Errorf("term %s exists in dictionary", tt.term)
 			}
 		})
 	}
